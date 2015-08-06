39f177f7fb03d85288ff549235b5a8b1427f14b6
ningyi.zny <ningyi.zny@alibaba-inc.com>
2015-08-06 13:28:57 +0800

    jianrong

--- event.js
+++ event.js
@@ -225,3 +225,9 @@ EventEmitter.prototype.listeners = function(type) {
   }
   return this._events[type];
 };
+
+EventEmitter.prototype.off = EventEmitter.prototype.removeAllListeners;
+EventEmitter.prototype.on = EventEmitter.prototype.addListener;
+EventEmitter.prototype.fire = EventEmitter.prototype.fireEvent = EventEmitter.prototype.emit;
+EventEmitter.prototype.clearAllEventListeners = EventEmitter.prototype.removeAllListeners;
+
